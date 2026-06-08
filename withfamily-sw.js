eeb8a2c28f508eff69e99dfae37cd3043d869f7e
Fan-Lin <150505581+Fan-Lin@users.noreply.github.com>
2026-06-08 03:25:23 -0400

    initial version

---
+++ withfamily-sw.js
@@ -0,0 +1,6 @@
+const CACHE='withfamily-v1';
+const ASSETS=['./'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.mode==='navigate')e.respondWith(fetch(e.request).catch(()=>caches.match('./')))});
+self.addEventListener('notificationclick',e=>{e.notification.close();e.waitUntil(clients.openWindow('./'))});
